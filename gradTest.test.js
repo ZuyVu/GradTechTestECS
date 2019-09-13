c88799ffa37f4f098d5119f555f0d9cdb837f104
Duy Vu <dtv15@imperial.co.uk>
2019-09-13 02:20:55 +0100

    Test passes

--- gradTest.test.js
+++ gradTest.test.js
@@ -1,5 +1,38 @@
-function createMenuData(data) {
+function getChildArray(menu, parent) {
 
+  for (j= 0; j < menu.length; j++) {
+    if(menu[j].title == parent) {
+      return menu[j].data;
+    }
+  }
+
+  var newParent = {title: parent, data: []}
+  menu.push(newParent);
+
+  return menu[menu.length - 1].data;
+
+}
+
+function createMenuData(data) {
+  var menu = [];
+
+  for (i = 0; i < data.length; i++) {
+    var parentAndChild = data[i].split("/");
+    var childs = getChildArray(menu, parentAndChild[0]);
+
+    if(parentAndChild[1] != null) {
+          childs.push(parentAndChild[1]);
+    }
+  }
+
+  var result = [];
+  for (i = 0; i < menu.length; i++) {
+    if(menu[i].data.length != 0) {
+      result.push(menu[i]);
+    }
+  }
+
+  return result;
 }
 
 describe("menu Data Generator", () => {
@@ -14,7 +47,7 @@ describe("menu Data Generator", () => {
         "parent3/parent3child1",
         "parent4"
       ];
-  
+
       const expectedResult = [
         {
           title: "parent1",
@@ -23,7 +56,7 @@ describe("menu Data Generator", () => {
         { title: "parent2", data: ["parent2child", "parent2child2"] },
         { title: "parent3", data: ["parent3child1"] }
       ];
-  
+
       const actualResult = createMenuData(data);
       expect(actualResult).toMatchObject(expectedResult);
     });
