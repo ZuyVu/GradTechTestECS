6dbb95dd4986b3d1ba7a4fa3192cdfba434a35cd
Duy Vu <dtv15@imperial.co.uk>
2019-09-13 10:22:25 +0100

    Added comments & fix indentation

--- gradTest.test.js
+++ gradTest.test.js
@@ -1,30 +1,29 @@
 function getChildArray(menu, parent) {
-
-  for (j= 0; j < menu.length; j++) {
+  /* Helper function which returns the data array of given parent name
+  from Array menu */
+  for (var j = 0; j < menu.length; j++) {
     if(menu[j].title == parent) {
       return menu[j].data;
     }
   }
-
+  // Create a new parent & push it to menu
   var newParent = {title: parent, data: []}
   menu.push(newParent);
-
   return menu[menu.length - 1].data;
-
 }
 
 function createMenuData(data) {
   var menu = [];
 
-  for (i = 0; i < data.length; i++) {
+  for (var i = 0; i < data.length; i++) {
     var parentAndChild = data[i].split("/");
     var childs = getChildArray(menu, parentAndChild[0]);
-
     if(parentAndChild[1] != null) {
           childs.push(parentAndChild[1]);
     }
   }
 
+  // Filtering out parents with empty child
   var result = [];
   for (i = 0; i < menu.length; i++) {
     if(menu[i].data.length != 0) {
